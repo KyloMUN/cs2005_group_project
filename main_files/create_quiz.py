77480a9801ba880e0834ee4f7955b7c0dc427522
Kyle Olmstead <kolmstead@mun.ca>
2019-04-03 15:53:10 -0230

    add createquiz module and test bench

--- main_files/create_quiz.py
+++ main_files/create_quiz.py
@@ -1,14 +1,12 @@
-"""This interacts with the Quiz and Question objects. Class CreateQuiz.
-
+"""This interacts with the Quiz and Question objects. Class CreateQuiz
 creates a new quiz object with the default attributes defined in the Quiz
 object class. Methods to add the start time, end time, time limit and
 questions to the quiz are provided in this class.
 """
 import datetime
-# from storage import Quiz, Question \
-#causes erroring when uncommented
-import shortuuid
-
+from persistence import Persistence
+from structures import Question, Quiz, QuestionBank
+from random import randint
 
 class Create:
     """Interface for quiz creation."""
@@ -16,9 +14,9 @@ class Create:
     def __init__(self, quizname, num_of_attempts):
         """Create a quiz object."""
         self.quiz = Quiz(quizname, num_of_attempts)
-        pass
+        self.persist = Persistence()
 
-    def add_start_time(self, year, month, day, hour=None, minutes=None):
+    def add_start_time(self, year, month, day, hour=0, minutes=0):
         """Allow authorized user to add the quiz start time.
 
         year -- int
@@ -27,10 +25,11 @@ class Create:
         hour -- int
         minutes -- int
         """
-        pass
+        datetimetemp = datetime.datetime(year, month, day, hour, minutes)
+        self.quiz.start_time = datetimetemp
 
-    def add_end_time(self, year, month, day, hour=None, minutes=None):
-        """Allow authorized user to add the quiz end time.
+    def add_end_time(self, year, month, day, hour=0, minutes=0):
+        """Allow authorizsd user to add the quiz end time.
 
         year -- int
         month -- int
@@ -38,47 +37,40 @@ class Create:
         hour -- int
         minutes -- int
         """
-        pass
+        datetimetemp = datetime.datetime(year, month, day, hour, minutes)
+        self.quiz.end_time = datetimetemp
 
     def add_time_limit(self, minutes):
         """Allow authorized user to add a quiz time limit."""
-        pass
+        self.quiz.time_limit = minutes
 
     def pass_to_storage(self):
         """Pass a quiz object to persistance for storage."""
-        pass
-
-    def add_question(self, questiontext, points, answerdict, choicesdict,
-                     display=None):
+        self.persist.store(self.quiz)
+        
+    def add_question(self, questiontext, points, answerdict, choicesdict):
         """Stub implementation of adding a question object to quiz.
 
         questiontext -- string of question
         points -- int, value of question
         answerdict -- the set of answers
         choicesdict -- the set of choices
-        display -- string
         """
-        pass
+        myquestion = Question(questiontext, points)
+        myquestion.answers = answerdict
+        myquestion.choices = choicesdict
+        self.quiz.questions.append(myquestion)
+        
 
     # methods relating to question bank
-
-    def add_all_quiz_questions_to_bank(self, qbank):
-        """Add all the questions from this quiz to the question bank for this course.
-
-        course_id -- course uuid
-        """
-        pass
-
-    def add_random_question_from_bank_to_quiz(self, qbank):
-        """Get a random question from the question bank and add it to the quiz.
-
-        course_id -- course uuid
-        """
-        pass
-
-    def add_question_to_bank(self, qbank, questionobj):
-        """Add a question to the question bank of this course id.
-
-        course_id -- course uuid
-        """
-        pass
+   
+    def add_all_questions_to_bank(self):
+        """Add a question to the question bank of this course id."""
+        tempbank = []
+        for questions in self.quiz.questions:
+            tempbank.append(questions)
+        Persistence.store(tempbank, QuestionBank)
+    
+    def get_random_question_from_bank(self, course_id):
+        qbank = self.persist.retrieve(QuestionBank, course_id)
+        return qbank[randint(0, len(qbank)-1)]
